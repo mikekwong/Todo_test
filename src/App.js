e409ae0ea3c75c6a923048fa57fa531038f0e93c
Mike Wong <wongmdesign@gmail.com>
2019-11-12 17:18:36 -0500

    added local storage

--- src/App.js
+++ src/App.js
@@ -1,63 +1,80 @@
-import React, { Component } from "react";
-import TodoBanner from "./TodoBanner";
-import TodoRow from "./TodoRow";
-import TodoCreator from "./TodoCreator";
-import VisibilityControl from "./VisibilityControl";
+import React, { Component } from 'react'
+import TodoBanner from './TodoBanner'
+import TodoRow from './TodoRow'
+import TodoCreator from './TodoCreator'
+import VisibilityControl from './VisibilityControl'
 
 export default class App extends Component {
   state = {
-    userName: "Adam",
+    userName: 'Adam',
     todoItems: [
-      { action: "Buy Flowers", done: false },
-      { action: "Get Shoes", done: false },
-      { action: "Collect Tickets", done: true },
-      { action: "Call Joe", done: false }
+      { action: 'Buy Flowers', done: false },
+      { action: 'Get Shoes', done: false },
+      { action: 'Collect Tickets', done: true },
+      { action: 'Call Joe', done: false }
     ],
     showCompleted: true
-  };
+  }
 
-  updateNewTextValue = e => {
-    this.setState({ newItemText: e.target.value });
-  };
+  componentDidMount () {
+    let data = localStorage.getItem('todos')
+    this.setState(
+      data !== null
+        ? JSON.parse(data)
+        : {
+          userName: 'Adam',
+          todoItems: [
+            { action: 'Buy Flowers', done: false },
+            { action: 'Get Shoes', done: false },
+            { action: 'Collect Tickets', done: true },
+            { action: 'Call Joe', done: false }
+          ],
+          showCompleted: true
+        }
+    )
+  }
 
   createNewTodo = task => {
     if (!this.state.todoItems.find(item => item.action === task)) {
-      this.setState({
-        todoItems: [...this.state.todoItems, { action: task, done: false }]
-      });
+      this.setState(
+        {
+          todoItems: [...this.state.todoItems, { action: task, done: false }]
+        },
+        () => localStorage.setItem('todos', JSON.stringify(this.state))
+      )
     }
-  };
+  }
 
   toggleTodo = todo => {
     this.setState({
       todoItems: this.state.todoItems.map(item =>
         item.action === todo.action ? { ...item, done: !item.done } : item
       )
-    });
-  };
+    })
+  }
 
   todoTableRows = doneValue =>
     this.state.todoItems
       .filter(item => item.done === doneValue)
       .map(item => (
         <TodoRow key={item.action} item={item} callback={this.toggleTodo} />
-      ));
+      ))
 
   changeStateData = () => {
     this.setState({
-      userName: this.state.userName === "Adam" ? "Bob" : "Adam"
-    });
-  };
+      userName: this.state.userName === 'Adam' ? 'Bob' : 'Adam'
+    })
+  }
 
-  render() {
-    const { userName, todoItems, newItemText, showCompleted } = this.state;
+  render () {
+    const { userName, todoItems, showCompleted } = this.state
     return (
       <div>
         <TodoBanner name={userName} tasks={todoItems} />
-        <div className="container-fluid">
-          <div className="my-1">
+        <div className='container-fluid'>
+          <div className='my-1'>
             <TodoCreator callback={this.createNewTodo} />
-            <table className="table table-striped table-bordered">
+            <table className='table table-striped table-bordered'>
               <thead>
                 <tr>
                   <th>Description</th>
@@ -66,14 +83,14 @@ export default class App extends Component {
               </thead>
               <tbody>{this.todoTableRows(false)}</tbody>
             </table>
-            <div className="bg-secondary text-white text-center p-2">
+            <div className='bg-secondary text-white text-center p-2'>
               <VisibilityControl
-                description="Completed Tasks"
+                description='Completed Tasks'
                 isChecked={showCompleted}
                 callback={checked => this.setState({ showCompleted: checked })}
               />
               {showCompleted && (
-                <table className="table table-striped table-bordered">
+                <table className='table table-striped table-bordered'>
                   <thead>
                     <tr>
                       <th>Description</th>
@@ -86,10 +103,10 @@ export default class App extends Component {
             </div>
           </div>
         </div>
-        <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+        <button className='btn btn-primary m-2' onClick={this.changeStateData}>
           Change
         </button>
       </div>
-    );
+    )
   }
 }
